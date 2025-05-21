33fbfc62351d3ab5f0d3641081dcb9766944ce34
Nicolás Pérez Stefan <nperezstefan@itba.edu.ar>
2025-05-21 17:27:37 -0300

    videoDriver
    clase videoDriver

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -3,6 +3,7 @@
 #include <lib.h>
 #include <moduleLoader.h>
 #include <naiveConsole.h>
+#include <videoDriver.h>
 
 extern uint8_t text;
 extern uint8_t rodata;
@@ -81,7 +82,7 @@ void * initializeKernelBinary()
 }
 
 int main()
-{	
+{
 	ncPrint("[Kernel Main]");
 	ncNewline();
 	ncPrint("  Sample code module at 0x");
